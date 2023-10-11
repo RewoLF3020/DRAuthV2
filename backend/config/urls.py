063436ea9de1e30e7a0432568c55e9e50d1ce5a0
RewoLF3020 <zolotovvladislav305@gmail.com>
2023-10-11 20:54:48 +0300

    api created

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -11,4 +11,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view()),
     path('api/token/refresh/', TokenRefreshView.as_view()),
     path('api/token/verify/', TokenVerifyView.as_view()),
+    path('api/users/', include('users.urls')),
 ]
---
+++ backend/users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import RegisterView, RetrieverUserView
+
+
+urlpatterns = [
+    path('register/', RegisterView.as_view()),
+    path('me/', RetrieverUserView.as_view()),
+]
